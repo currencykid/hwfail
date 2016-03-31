cb0933022b169851c97d8f68c251835682dadd0e
ck <thecurrencykid@gmail.com>
2016-03-31 14:04:29 -0700

    added votable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show,:edit,:update,:destroy]
+  before_action :find_post, only: [:show,:edit,:update,:destroy, :upvote]
   def index
     @posts = Post.all.order("created_at DESC")  
   end 
@@ -38,6 +38,11 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to root_path 
   end 
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_to :back
+  end
 
   
   private
