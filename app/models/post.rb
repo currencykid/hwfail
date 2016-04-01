291dda4bafff5c2607620b96131668c49a522ef1
ck <thecurrencykid@gmail.com>
2016-03-31 17:15:49 -0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: {medium: "300x300>" }
    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/                                     
+	validates :title, presence: true, length: {minimum: 1, maximum: 50}
+	validates :description, presence: true, length:  {minimum: 3, maximum: 1000}
+  validates :image, presence: true 
 end
